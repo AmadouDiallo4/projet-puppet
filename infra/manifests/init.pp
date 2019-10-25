c89e7e1897dacf6fcfbf25c23a6a1070095438e6
Vagrant Default User <vagrant@control>
2019-10-25 13:17:21 +0000

    modif script

--- infra/manifests/init.pp
+++ infra/manifests/init.pp
@@ -8,13 +8,8 @@ node default {
 		}
 
 node server0 {
-	class { 'apache':
-		port => '80',
-		docroot => /var/www/vhost'
-		ensure => installed,
-		docroot_owner => 'www-data',
-		docroot_group => 'www-data',
-	
+	package { 'apache':
+		ensure => installed,	
 		
 	}
 
